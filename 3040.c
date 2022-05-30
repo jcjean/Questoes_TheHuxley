d360e75d3b8679f5aefe71ed09a96aec59ea00c2
Jean <104872875+jcjean@users.noreply.github.com>
2022-05-30 05:24:19 +0000

    Ho Ho Ho!

---
+++ 3040.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+    int qtde, n;
+    
+    scanf("%i", &qtde);
+    
+    for (n=1; n<qtde; n++){
+        printf ("Ho ");
+    }
+    printf("Ho!");
+    
+	return 0;
+}
