80502a30f31f01f5ab9cda5d5cd802b3533a67b7
Jean <104872875+jcjean@users.noreply.github.com>
2022-06-07 23:27:32 +0000

    Algarismo da casa das unidades

---
+++ 166.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+	int x, u;
+    
+  scanf("%d", &x);
+    
+  u= x%10;	
+    
+  printf("%d", u);
+    
+	return 0;
+}
